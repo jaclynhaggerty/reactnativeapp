596d377b4c0982186e33b343e7c090400dcec6bd
JACLYNHAGGERTY <JACLYNHAGGERTY@GMAIL.com>
2022-09-20 12:23:41 +0200

    commit of app

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from "react";
+import { Text, View, ActivityIndicator, Image, ScrollView, TouchableOpacity } from "react-native";
+import { Alert } from "react-native";
+import { Button } from "react-native";
 
 export default function App() {
+  const randomNum = useRef(Math.random()).current;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
+        <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
+          Hello React Native
+        </Text>
+        <ActivityIndicator
+          size="large"
+          color="#c1262c"
+          style={{ marginBottom: 30 }}
+        />
+        <Button
+          onPress={() => Alert.alert("Learning RN is so easy")}
+          title="Learn More"
+          color="#c1262c"
+        />
+       
+          {[0, 1, 2, 3, 4].map(i => {
+            return (
+              <TouchableOpacity
+                key={i}
+                onPress={() => Alert.alert(`You pressed image #${i + 1}`)}
+                >
+                <Image
+                source={{
+                  uri: `https://picsum.photos/500/300?random=${randomNum + i}`
+                }}
+                style={{ width: "100%", height: 160, marginBottom: 30 }}
+              />
+              </TouchableOpacity>
+            );
+          })}
+        <View
+          style={{
+            borderWidth: 2,
+            borderColor: "black",
+            padding: 20,
+            marginBottom: 30
+
+          }}
+        >
+          <Text> Hello again! </Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
