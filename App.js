3a29acd2c29b527832a05b6e3c91f3277292b588
JACLYNHAGGERTY <JACLYNHAGGERTY@GMAIL.com>
2022-09-20 14:14:32 +0200

    added text box

--- App.js
+++ App.js
@@ -1,55 +1,81 @@
 import React, { useRef } from "react";
-import { Text, View, ActivityIndicator, Image, ScrollView, TouchableOpacity } from "react-native";
+import { Text, View, ActivityIndicator, Image, TouchableOpacity, FlatList, TextInput, StyleSheet, onChangeText } from "react-native";
 import { Alert } from "react-native";
 import { Button } from "react-native";
+import { useState } from "react";
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+function MyHeader() {
+  const [myText, setMyText] = React.useState("");
+  return (
+    <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
+      <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
+        Hello React Native
+      </Text>
+      {/* <ActivityIndicator
+        size="large"
+        color="#c1262c"
+        style={{ marginBottom: 30 }}
+      /> */}
+      <Button
+        onPress={() => Alert.alert(myText)}
+        title="Show Input"
+        color="#c1262c"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setMyText}
+        value={myText}
+      />
+    </View>
+  )
+}
+function MyFooter() {
+  return (
+    <View
+      style={{
+        borderWidth: 2,
+        borderColor: "black",
+        padding: 20,
+        marginBottom: 30
+      }}
+    >
+      <Text> Hello again! </Text>
+    </View>
+  )
+}
 
 export default function App() {
   const randomNum = useRef(Math.random()).current;
   return (
-    <ScrollView>
-      <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
-        <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
-          Hello React Native
-        </Text>
-        <ActivityIndicator
-          size="large"
-          color="#c1262c"
-          style={{ marginBottom: 30 }}
-        />
-        <Button
-          onPress={() => Alert.alert("Learning RN is so easy")}
-          title="Learn More"
-          color="#c1262c"
-        />
-       
-          {[0, 1, 2, 3, 4].map(i => {
-            return (
-              <TouchableOpacity
-                key={i}
-                onPress={() => Alert.alert(`You pressed image #${i + 1}`)}
-                >
-                <Image
-                source={{
-                  uri: `https://picsum.photos/500/300?random=${randomNum + i}`
-                }}
-                style={{ width: "100%", height: 160, marginBottom: 30 }}
-              />
-              </TouchableOpacity>
-            );
-          })}
-        <View
-          style={{
-            borderWidth: 2,
-            borderColor: "black",
-            padding: 20,
-            marginBottom: 30
-
-          }}
-        >
-          <Text> Hello again! </Text>
-        </View>
-      </View>
-    </ScrollView>
+    <FlatList
+      ListHeaderComponent={MyHeader}
+      data={[0, 1, 2, 3, 4]}
+      renderItem={({ item }) => {
+        return (
+          <TouchableOpacity
+            onPress={() => Alert.alert(`You pressed image #${item + 1}`)}
+          >
+            <Image
+              source={{
+                uri: `https://picsum.photos/500/300?random=${randomNum + item}`
+              }}
+              style={{ width: "100%", height: 160, marginBottom: 30 }}
+            />
+          </TouchableOpacity>
+        );
+      }}
+      keyExtractor={item => String(item)}
+      ListFooterComponent={MyFooter}
+    />
   );
 }
 
